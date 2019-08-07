e6957dbab1b9d5b1792ea92be2b1dcf61c8dad84
kasimebrahim <se.kasim.ebrahim@gmail.com>
2019-08-07 18:47:55 +0300

    Filter Globs

--- opencog/atoms/core/TypeUtils.cc
+++ opencog/atoms/core/TypeUtils.cc
@@ -299,7 +299,7 @@ Handle filter_vardecl(const Handle& vardecl, const HandleSeq& hs)
 		return Handle::UNDEFINED;
 
 	Type t = vardecl->get_type();
-	if (VARIABLE_NODE == t)
+	if ((VARIABLE_NODE == t) || (GLOB_NODE == t))
 	{
 		if (is_free_in_any_tree(hs, vardecl))
 			return vardecl;
@@ -311,7 +311,7 @@ Handle filter_vardecl(const Handle& vardecl, const HandleSeq& hs)
 	{
 		Handle var = vardecl->getOutgoingAtom(0);
 		Type t = var->get_type();
-		if (t == VARIABLE_NODE and filter_vardecl(var, hs))
+		if (((t == VARIABLE_NODE) || (t == GLOB_NODE)) and filter_vardecl(var, hs))
 			return vardecl;
 	}
 
